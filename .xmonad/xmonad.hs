22f6d07dd106d9f022385f4825b4c6c8587bce40
Santiago Torres <torresariass@gmail.com>
2017-05-15 23:10:35 -0400

    FIX: xmonad: fixes xmobar and adds gaps

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -2,65 +2,56 @@ import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
--- import XMonad.Actions.Volume
 import XMonad.Util.EZConfig
 import Graphics.X11.ExtraTypes.XF86
+import XMonad.Layout.Tabbed
 import System.IO
+
+import XMonad.Layout.NoBorders (smartBorders)
+import qualified XMonad.Layout.BinarySpacePartition as BSP
+import qualified XMonad.Layout.Spacing as S
 
 myManageHook = composeAll [
     className =? "trayer" --> doFloat
-   , manageDocks
+    , manageDocks
     ]
+
+myTerminal = "termite"
 
 main = do
     xmproc <- spawnPipe "xmobar"
-    xmonad $ defaultConfig {
-    terminal   = "termite",
-        manageHook = myManageHook, 
-        layoutHook = avoidStruts $ layoutHook defaultConfig,
-        logHook = dynamicLogWithPP xmobarPP
+    xmonad $ def {
+        terminal = myTerminal
+        , manageHook = myManageHook
+        , layoutHook = myLayout
+        , logHook = dynamicLogWithPP xmobarPP
             { ppOutput = hPutStrLn xmproc,
-            ppTitle = xmobarColor "Blue" "" . shorten 50
-            },
---        workspaces = withScreens 2 ["web", "email", "irc"] ,
-        modMask = mod4Mask -- Rebind Mod to the Windows key
+            ppTitle = xmobarColor "Green" "" . shorten 50
+            }
+        , handleEventHook = mconcat [ docksEventHook
+                                    , handleEventHook def ]
+        , modMask = mod4Mask 
         } `additionalKeys`
-        [ ((mod4Mask .|. shiftMask, xK_l), spawn "xscreensaver-command -lock") -- mod + shift + l locks
-        , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s") -- screenshot current image
-        , ((0, xK_Print), spawn "scrot") -- screenshot whole window
-
-        -- change keyboard layouts
-        -- From:
-        -- askubuntu.com/questions/132616/
-        -- how-do-i-configure-keyboard-layouts-using-xmonad-as-my-window-manager
+        [ 
+        ((mod4Mask .|. shiftMask, xK_l), spawn "xscreensaver-command -lock") 
+        , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s") 
+        , ((0, xK_Print), spawn "scrot") 
         , ((mod1Mask .|. shiftMask, xK_z), spawn "setxkbmap -layout us")
         , ((mod1Mask .|. shiftMask, xK_x), spawn "setxkbmap -layout latam")
-
-        -- adds a notification telling me what color im looking at
-        --, ((mod4mask, xK_c), spawn "notify-send $(/home/santiago/.xmonad/whatsthiscolor)")
         , ((mod4Mask .|. shiftMask, xK_p), spawn "/home/santiago/.xmonad/notify-color.sh")
 
-        -- volume keys
+        -- function keys, sleep, etc.
         , ((0, xF86XK_AudioLowerVolume) , spawn "pamixer -d 3")
         , ((0, xF86XK_AudioRaiseVolume) , spawn "pamixer -i 3")
-
-        -- pavucontrol gets in the way
         , ((0, xF86XK_AudioMute), spawn "pamixer -t")
-
-        -- closing the lid
-        --        , ((0, xF86XK_Sleep), spawn "/home/santiago/.xmonad/sleep.sh")
-        --  this will be done kernel-wise by modifying: /etc/acpi/events/lidbtn
-        --   change the script from lid.sh to sleep.sh
         , ((mod4Mask .|. shiftMask, xK_s), spawn "/home/santiago/.xmonad/sleep")
-
-        -- let's change brightness, check the adjust_brightness script
-    	--  to see how this is done... the commands executed are just
-        --  simple setuid wrappers for the script
-        , ((0, xF86XK_MonBrightnessUp),
-            spawn "/home/santiago/.xmonad/raise_brightness")
-        , ((0, xF86XK_MonBrightnessDown),
-            spawn "/home/santiago/.xmonad/lower_brightness")
+        , ((0, xF86XK_MonBrightnessUp), spawn "/home/santiago/.xmonad/raise_brightness")
+        , ((0, xF86XK_MonBrightnessDown), spawn "/home/santiago/.xmonad/lower_brightness")
         ]
 
-
-
+myLayout = avoidStruts(S.spacing 1 $ (
+                Full |||
+                Tall 1 (3/100) (1/2) |||
+                Mirror (Tall 1 (3/100) (1/2))
+                )
+            )
